0fe23daf800b20ed0547bd7be9d8e7140101ac16
vitruvianAnalogy <nishantrawat22@gmail.com>
2015-09-18 23:40:54 -0700

    Interpolation Search added

--- Searching/InterpolationSearch.java
+++ Searching/InterpolationSearch.java
@@ -1,8 +1,42 @@
+/*In interpolation search, the interpolation is used to find an item near the
+one being searched for, then linear search is ussed to find the exact item*/
 import java.util.*;
 public class InterpolationSearch{
 
-		public static boolean search(int[] a, int element){
+		public static int search(int[] a, int element){
+			int low = 0;
+			int high = a.length-1;
+			int mid;
 
+			while(a[low]<element && a[high]>element){
+				if(a[high]-a[low]==0)
+					return -1;
+
+				/*The middle will be greater than low . How much greater?
+				The high-low will tell how many observations are between them
+				The a[high]-a[low] will tell what is the difference between
+				the observations VALUE. So, their ratio would mean, how
+				much VALUE is spread between those two observations.Multiply
+				that ratio to difference between low and element, so that
+				we know how many observations far is element*/
+				mid = low + (element-a[low])*(high-low)/(a[high]-a[low]);
+
+				//We make mid as the lower value, kind of like binary search
+				if(a[mid]<element){
+					low = mid + 1;
+				}
+				else if(a[mid]>element)
+					high = mid - 1;
+				else
+					return mid;
+			}
+
+			if(a[low]==element)
+				return low;
+			else if (a[high]==element)
+				return high;
+			else
+				return -1;
 		}
 
 		public static void main(String[] args){
@@ -23,9 +57,16 @@ public class InterpolationSearch{
 
 		System.out.println("Enter which number is to be searched");
 		int element = sn.nextInt();
+		int temp = search(a,element);
 
-		if(!search(a,element)){
+		if(temp ==-1){
 			System.out.println("Couldn't find the element");
 		}
+		else
+		{
+			temp++;
+			System.out.println("Element at "+ (temp)+" position");
+		}
+			
 	}
 }
---
+++ Sorting/MergeSortLL.java
@@ -0,0 +1,5 @@
+public class MergeSortLL{
+	public static void main(String[] args){
+		
+	}
+}
